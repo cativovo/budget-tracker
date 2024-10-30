d544216263b8818166e5f9d5f9cc119009d7c0a8
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-10-30 23:31:59 +0800

    add navbar

---
+++ js/icons.js
@@ -0,0 +1,18 @@
+import {
+  createIcons,
+  ChartPie,
+  ScrollText,
+  UserRound,
+  Plus,
+  NotebookText,
+} from "lucide";
+
+createIcons({
+  icons: {
+    NotebookText,
+    ChartPie,
+    Plus,
+    ScrollText,
+    UserRound,
+  },
+});
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import "vite/modulepreload-polyfill";
 import htmx from "htmx.org";
+import "./icons";
 import "../css/index.css";
 
 window.htmx = htmx;
