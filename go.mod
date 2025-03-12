b71271db16bee07400e0fd72a252917f4b6e079b
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-03-12 12:34:19 +0800

    add validator

--- go.mod
+++ go.mod
@@ -35,8 +35,12 @@ require (
 	github.com/elastic/go-windows v1.0.1 // indirect
 	github.com/fatih/color v1.17.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.25.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gohugoio/hugo v0.134.3 // indirect
@@ -54,6 +58,7 @@ require (
 	github.com/klauspost/compress v1.17.10 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
@@ -83,11 +88,11 @@ require (
 	go.opentelemetry.io/otel v1.28.0 // indirect
 	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
-	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/tools v0.25.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240812133136-8ffd90a71988 // indirect
---
+++ internal/validator/validator.go
@@ -0,0 +1,88 @@
+package validator
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// https://github.com/go-playground/validator/issues/559#issuecomment-1871786235
+type validationErrors []error
+
+func (v validationErrors) Error() string {
+	var message string
+
+	for i, err := range v {
+		if i > 0 {
+			message += ","
+		}
+		message += err.Error()
+	}
+
+	return message
+}
+
+type Validator struct {
+	validator *validator.Validate
+}
+
+func (v *Validator) Struct(s any) error {
+	if err := v.validator.Struct(s); err != nil {
+		var vErrs validationErrors
+
+		for _, err := range err.(validator.ValidationErrors) {
+			var e error
+			switch err.Tag() {
+			case "required":
+				e = fmt.Errorf("'%s' is required", err.Field())
+			case "required_with":
+				if field, ok := reflect.TypeOf(s).Elem().FieldByName(err.Param()); ok {
+					if jsonTag, ok := field.Tag.Lookup("json"); ok {
+						e = fmt.Errorf("'%s' is required with '%s'", err.Field(), jsonTag)
+					}
+				}
+			case "number":
+				e = fmt.Errorf("'%s' must have a valid numeric value", err.Field())
+			case "hexcolor":
+				e = fmt.Errorf("'%s' must have a valid hex color value", err.Field())
+			case "datetime":
+				e = fmt.Errorf("'%s' must have a valid date value", err.Field())
+			case "gte":
+				e = fmt.Errorf("'%s' must be greater than or equal to %s", err.Field(), err.Param())
+			case "gt":
+				e = fmt.Errorf("'%s' must be greater than %s", err.Field(), err.Param())
+			default:
+				e = fmt.Errorf("'%s': '%v' must satisfy '%s' '%v' criteria", err.Field(), err.Value(), err.Tag(), err.Param())
+			}
+			vErrs = append(vErrs, e)
+		}
+
+		return vErrs
+	}
+
+	return nil
+}
+
+func (v *Validator) Var(f any, tag string) error {
+	return v.validator.Var(f, tag)
+}
+
+func NewValidator() *Validator {
+	v := validator.New(validator.WithRequiredStructEnabled())
+
+	// https://github.com/go-playground/validator/issues/861
+	v.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		// skip if tag key says it should be ignored
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+
+	return &Validator{
+		validator: v,
+	}
+}
