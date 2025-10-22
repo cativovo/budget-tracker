090a6f9b54d53953700de1dabf3c118092d5d078
Mark Leo Cativo <cativomarkleo@gmail.com>
2025-10-22 17:24:49 +0800

    update key and comments

--- internal/log/context.go
+++ internal/log/context.go
@@ -8,18 +8,18 @@ import (
 // The provided key must be comparable and should not be of type string
 // or any other built-in type to avoid collisions between packages using context.
 // Users of WithValue should define their own types for keys.
-type contextKey string
+type ctxKey string
 
-const contextKeyLogger contextKey = "logger"
+const ctxKeyLogger ctxKey = "logger"
 
-// WithContext returns a context with the given logger
+// WithContext returns a context with the given logger.
 func WithContext(ctx context.Context, logger *slog.Logger) context.Context {
-	return context.WithValue(ctx, contextKeyLogger, logger)
+	return context.WithValue(ctx, ctxKeyLogger, logger)
 }
 
-// FromContext extracts logger from context
+// FromContext extracts logger from context.
 func FromContext(ctx context.Context) *slog.Logger {
-	l, ok := ctx.Value(contextKeyLogger).(*slog.Logger)
+	l, ok := ctx.Value(ctxKeyLogger).(*slog.Logger)
 	if !ok {
 		return slog.Default()
 	}
